fba4c452fc47a8681df17148d857a5cddee2af4a
Nate Levesque <public@thenaterhood.com>
2014-11-05 22:23:54 -0500

    Add documentation to network stuff

--- network/__init__.py
+++ network/__init__.py
@@ -3,7 +3,20 @@ import socket
 import threading
 from queue import Queue, Empty
 
-def get_local_ip():
+class NetworkInfo():
+    """
+    Contains network data
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        self.ip_lan = self.get_local_ip()
+        self.hostname = socket.gethostname()
+        self.fqdn = socket.getfqdn()
+
+    def get_local_ip(self):
         def udp_listening_server():
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             s.bind(('<broadcast>', 8888))
@@ -24,7 +37,7 @@ def get_local_ip():
         s2.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         waiting = True
         while waiting:
-            s2.sendto(bytes('What is my LAN IP address?', 'UTF-8'), ('<broadcast>', 8888))
+            s2.sendto(bytes('Whats my LAN IP', 'UTF-8'), ('<broadcast>', 8888))
             try:
                 address = queue.get(False)
             except Empty:
@@ -33,36 +46,60 @@ def get_local_ip():
                 waiting = False
         return address[0]
 
-class NetworkInfo():
-    def __init__(self):
-        self.ip_lan = get_local_ip()
-        self.hostname = socket.gethostname()
-        self.fqdn = socket.getfqdn()
 
 class SocketListener(threading.Thread):
+    """
+    Listens on a socket and calls back when
+    data is received.
 
-    def __init__(self, port, callback):
+    Extends threading.Thread
+    """
+
+    def __init__(self, port, callback, daemon = True):
+        """
+        Constructor
+
+        Parameters:
+            int      port:     The port to listen to
+            Function callback: The function to call when data is received
+            bool     daemon:   Whether to run as a daemon thread
+        """
         self.port = port
         self.callback = callback
 
         listen = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         listen.bind(('', self.port))
         self.listen_socket = listen
-        super().__init__(daemon = True)
+        super().__init__(daemon = daemon)
 
     def _listen(self):
+        """
+        Listens for data then calls back when data is received
+        """
         data, addr = self.listen_socket.recvfrom(1024)
         self.callback(data, addr)
 
     def run(self):
+        """
+        Runs the thread (typically with the Thread start() method)
+        """
         while True:
             self._listen()
 
 class SocketBroadcaster():
+    """
+    Sends a broadcast packet containing data on a given port
+    """
 
     __slots__ = ('_bcast_socket', '_port')
 
     def __init__(self, port):
+        """
+        Constructor
+
+        Params:
+            int port: The port to broadcast on
+        """
         bcast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         bcast.bind(('', 0))
         bcast.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -70,7 +107,14 @@ class SocketBroadcaster():
         self._bcast_socket = bcast
 
     def push(self, data):
+        """
+        Sends a broadcast
+
+        Params:
+            binary data: Encoded data to send
+        """
         self._bcast_socket.sendto(data, ('<broadcast>', self._port))
 
 if __name__ == '__main__':
-    print(get_local_ip())
+    netinfo = NetworkInfo()
+    print(netinfo.ip_lan)
