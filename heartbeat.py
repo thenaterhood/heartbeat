6a3d6ab1b6cdb23c33d95b2dc4e8f1616664d2ec
Nate Levesque <public@thenaterhood.com>
2014-11-05 22:24:23 -0500

    Make it possible to shut down cleanly

--- heartbeat.py
+++ heartbeat.py
@@ -1,6 +1,14 @@
 from modules import Heartbeat
 import settings
+import sys
 
-server = Heartbeat(settings.PORT, 2, settings.SECRET_KEY)
-server.start()
-print("Heartbeat started")
+if __name__ == "__main__":
+    try:
+        server = Heartbeat(settings.PORT, 2, settings.SECRET_KEY)
+        server.daemon = True
+        server.start()
+        print("Heartbeat started. Hit ctrl+c to stop.")
+        server.join()
+    except (KeyboardInterrupt):
+        print("Caught ctrl+c, shutting down Heartbeat...")
+        sys.exit(0)
--- monitor.py
+++ monitor.py
@@ -1,10 +1,18 @@
 from modules import Monitor
 from modules import Heartbeat
 import settings
+import sys
 
-listener = Monitor(settings.PORT, settings.SECRET_KEY, settings.NOTIFIERS)
-listener.start()
-print( "Monitor started" )
+if __name__ == "__main__":
+    try:
+        listener = Monitor(settings.PORT, settings.SECRET_KEY, settings.NOTIFIERS)
+        listener.daemon = True
+        listener.start()
+        print("Monitor started. Use ctrl+c to stop.")
+        listener.join()
+    except KeyboardInterrupt:
+        print("ctrl+c received, shutting down monitor")
+        sys.exit(0)
 #heart = Heartbeat(settings.PORT, 2, settings.SECRET_KEY)
 #heart.start()
 #print("Started monitor heartbeat")
