6ec4ca60739926832ef571cb5f0515997a5caa24
Nate Levesque <public@thenaterhood.com>
2014-11-08 11:44:04 -0500

    Add notifier that prints the messages to the console

---
+++ notifiers/stdout.py
@@ -0,0 +1,17 @@
+from notifiers import NotifyWorker
+from datetime import datetime
+
+class PrintOutput(NotifyWorker):
+
+    __slots__ = ('message', 'title')
+
+    def __init__(self, event):
+        host = event.host
+        self.title = event.title + ": " + host
+        date = datetime.now()
+        self.message = host + ": " + event.message + " at " + event.timestamp.strftime("%H:%M:%S %m/%d/%y")
+        super(PrintOutput, self).__init__(event)
+
+    def run(self):
+        # Push a notification to wherever
+        print(self.title + ": " + self.message)
