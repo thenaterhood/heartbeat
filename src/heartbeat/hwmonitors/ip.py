b71f86c5fb410ee3a51c74355566b637f6df5f49
Nate Levesque <public@thenaterhood.com>
2014-11-30 13:51:35 -0500

    add wan/lan ip monitors

---
+++ src/heartbeat/hwmonitors/ip.py
@@ -0,0 +1,29 @@
+from heartbeat.hwmonitors import MonitorWorker
+from heartbeat.notifiers import Event
+from heartbeat.network import NetworkInfo
+
+
+class LANIp(MonitorWorker):
+
+    def __init__(self, callback):
+        super(LANIp, self).__init__(callback)
+
+    def run(self):
+        net = NetworkInfo()
+
+        event = Event(
+            "New LAN IP", "LAN IP address is now " + net.ip_lan, net.fqdn)
+
+        self.callback(event)
+
+
+class WANIp(MonitorWorker):
+
+    def __init__(self, callback):
+        super(WANIp, self).__init__(callback)
+
+    def run(self):
+        net = NetworkInfo()
+        event = Event(
+            "New WAN IP", "WAN IP address is now " + net.ip_wan, net.fqdn)
+        self.callback(event)
