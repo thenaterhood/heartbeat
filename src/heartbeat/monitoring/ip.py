9c1b70ab32dbf80f8168831aa72edeb1f9b5d2e2
Nate Levesque <public@thenaterhood.com>
2014-12-21 21:37:42 -0500

    Add statefulness to ip monitors, only throw event on IP change

--- src/heartbeat/monitoring/ip.py
+++ src/heartbeat/monitoring/ip.py
@@ -7,26 +7,30 @@ class LANIp(Monitor):
 
     def __init__(self, callback):
         super(LANIp, self).__init__(callback)
+        self.ipv4 = '0.0.0.0'
 
     def run(self):
         net = NetworkInfo()
+        if (net.ip_lan != self.ipv4):
+            self.ipv4 = net.ip_lan
+            event = Event(
+                "New LAN IP", "LAN IP address is now " + net.ip_lan, net.fqdn)
+            event.one_time = True
 
-        event = Event(
-            "New LAN IP", "LAN IP address is now " + net.ip_lan, net.fqdn)
-        event.one_time = True
-
-        self.callback(event)
-
+            self.callback(event)
 
 class WANIp(Monitor):
 
     def __init__(self, callback):
         super(WANIp, self).__init__(callback)
+        self.ipv4 = '0.0.0.0'
 
     def run(self):
         net = NetworkInfo()
-        event = Event(
-            "New WAN IP", "WAN IP address is now " + net.ip_wan, net.fqdn)
-        event.one_time = True
+        if (net.ip_wan != self.ipv4):
+            self.ipv4 = net.ip_wan
+            event = Event(
+                "New WAN IP", "WAN IP address is now " + net.ip_wan, net.fqdn)
+            event.one_time = True
 
-        self.callback(event)
+            self.callback(event)
