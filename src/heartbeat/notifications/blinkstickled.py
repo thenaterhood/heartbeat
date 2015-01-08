369945020fb9ec8cd0a0c1ee803bf756d55193d1
Nate Levesque <public@thenaterhood.com>
2015-01-07 23:12:16 -0500

    Add notifier for changing blinkstick colors

---
+++ src/heartbeat/notifications/blinkstickled.py
@@ -0,0 +1,49 @@
+from blinkstick import blinkstick
+from heartbeat.notifications import Notifier
+from heartbeat.platform import Configuration, EventType
+from datetime import datetime
+
+
+class BlinkStickColor(Notifier):
+
+    __slots__ = ('message', 'title', 'serial', 'warning', 'okay', 'alert', 'use_color', 'previous_warnings')
+
+    def __init__(self):
+        config = Configuration()
+        self.previous_warnings = {}
+        self.serial = config.config['heartbeat.notifiers.blinkstick']['serial']
+        self.warning = config.config['heartbeat.notifiers.blinkstick']['warning_color']
+        self.okay = config.config['heartbeat.notifiers.blinkstick']['okay_color']
+        self.alert = config.config['heartbeat.notifiers.blinkstick']['alert_color']
+        super(BlinkStickColor, self).__init__()
+
+    def _choose_color(self, event):
+        if (isinstance(event.type, EventType.warn) or isinstance(event.type, EventType.error)):
+            self.use_color = self.alert
+            self.previous_warnings[event.source] = event.datestamp
+
+        elif (isinstance(event.type, EventType.info)):
+            if (event.source in self.previous_warnings):
+                del(self.previous_warnings[event.source])
+
+            if (len(self.previous_warnings) < 1):
+                self.use_color = self.okay
+
+        else:
+            self.use_color = self.okay
+
+    def load(self, event):
+        host = event.host
+        self.title = event.title + ": " + host
+        date = datetime.now()
+        self.message = host + ": " + event.message + " at " + \
+            event.timestamp.strftime("%H:%M:%S %m/%d/%y")
+
+        self._choose_color(event)
+
+    def run(self):
+        bstick = blinkstick.find_by_serial(self.serial)
+        if (bstick == None):
+            print("Blinkstick not found")
+        else:
+            bstick.set_color(hex=self.use_color)
