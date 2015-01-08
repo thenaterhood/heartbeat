bd1d36c5626a21bb4d5f4b9fc9636e44212fed76
Nate Levesque <public@thenaterhood.com>
2015-01-07 22:44:09 -0500

    update notifier template

--- src/heartbeat/notifications/Template.py
+++ src/heartbeat/notifications/Template.py
@@ -6,13 +6,15 @@ class NotifyTemplate(Notifier):
 
     __slots__ = ('message', 'title')
 
-    def __init__(self, event):
+    def __init__(self):
+        super(NotifyTemplate, self).__init__()
+
+    def load(self, event):
         host = event.host
         self.title = event.title + ": " + host
         date = datetime.now()
         self.message = host + ": " + event.message + " at " + \
             event.timestamp.strftime("%H:%M:%S %m/%d/%y")
-        super(NotifyTemplate, self).__init__(event)
 
     def run(self):
         # Push a notification to wherever
