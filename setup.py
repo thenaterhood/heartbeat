f8e35ee272e659da5e287b0a486b3771a0029294
Nate Levesque <public@thenaterhood.com>
2015-01-21 16:15:49 -0500

    Bump version numbers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Heartbeat',
-    version='1.0.1',
+    version='1.2.1',
     description='Heartbeat monitoring tool',
     author='Nate Levesque',
     author_email='public@thenaterhood.com',
