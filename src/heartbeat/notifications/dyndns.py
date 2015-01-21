6225ac958b69052d80acab0c965b2d88c2d18ba2
Nate Levesque <public@thenaterhood.com>
2015-01-21 15:18:02 -0500

    Add dynamic dns url pull

---
+++ src/heartbeat/notifications/dyndns.py
@@ -0,0 +1,33 @@
+from heartbeat.notifications import Notifier
+from heartbeat.platform import Configuration
+import urllib.parse
+import urllib.request
+
+
+class UrlPull(Notifier):
+
+    __slots__ = ('current_ip', 'new_ip', 'url')
+
+    def __init__(self):
+        self.current_ip = None
+        self.new_ip = None
+        config = Configuration()
+        self.url = config.config['heartbeat.notifiers.dyndns']['urlpull']
+        super(UrlPull, self).__init__()
+
+    def load(self, event):
+
+        if ('ip_type' in event.payload
+                and event.payload['ip_type'] == 'WAN'
+                ):
+            ip = event.payload['ip']
+            if (ip != self.current_ip):
+                self.new_ip = ip
+
+    def run(self):
+
+        try:
+            urllib.request.urlopen(self.url, timeout=5)
+
+        except:
+            pass
