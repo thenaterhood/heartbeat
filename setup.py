ad735f8f1389c30880dabd98602539763a39245d
Nate Levesque <public@thenaterhood.com>
2015-01-22 23:02:03 -0500

    Add enum34 as requirement for <3.4, add suggestions

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import sys
+
+install_requires = []
+suggested = {
+    'Pushbullet': ["pushbullet.py >= 0.5"],
+    'Blinkstick': ["blinkstick >= 1.1.6"]
+        }
+
+if (sys.version_info < (3, 4)):
+    install_requires.append('enum34 >= 1.0')
 
 setup(name='Heartbeat',
     version='2.0.0',
@@ -8,6 +18,7 @@ setup(name='Heartbeat',
     author='Nate Levesque',
     author_email='public@thenaterhood.com',
     url='https://github.com/thenaterhood/heartbeat/archive/master.zip',
+    requires=install_requires,
     package_dir={'':'src'},
     packages=[
         'heartbeat',
@@ -22,3 +33,9 @@ setup(name='Heartbeat',
         ('etc', ['dist/_etc/heartbeat.yml']),
         ]
     )
+
+print()
+print("====================> Suggested Packages <=====================")
+for k in suggested:
+    print("        " + k + ", satisfiable by " + str(suggested[k]))
+print()
