7ebd0139a0b0f75dc34023c1a66fdb3a1485b0ac
Nate Levesque <public@thenaterhood.com>
2015-04-28 17:12:02 -0400

    Improvements to testing threading

--- test/multiprocessing/test_multiprocessing.py
+++ test/multiprocessing/test_multiprocessing.py
@@ -7,11 +7,14 @@ else:
     from unittest.mock import MagicMock
     from unittest.mock import Mock
 from heartbeat.multiprocessing import Worker
+from heartbeat.multiprocessing import LockingDictionary
 from queue import Queue
 
 class WorkerTest(unittest.TestCase):
 
     def setUp(self):
+        with open('derp.log', 'a') as derp:
+            derp.write("Starting a new test\n")
         self.counter = 0
         self.exception_counter = 0
         self.tasks = 3
@@ -27,8 +30,7 @@ class WorkerTest(unittest.TestCase):
         raise Exception("Testing")
 
     def worker_callback(self):
-        if (self.counter == self.tasks):
-            self.counter += 1
+        if (self.counter >= self.tasks):
             self.worker.shutdown = True
             self.queue.put((self.worker_callback, [], {}))
         else:
@@ -45,7 +47,7 @@ class WorkerTest(unittest.TestCase):
     def test_run_once(self):
         self.tasks = 1
         self.queue.put((self.worker_callback, [], {}))
-        self.worker.start()
+        self.worker.run()
 
         self.assertEqual(self.counter, 1)
 
@@ -53,7 +55,7 @@ class WorkerTest(unittest.TestCase):
         self.tasks = 2
         self.queue.put((self.worker_callback, [], {}))
         self.queue.put((self.worker_callback, [], {}))
-        self.worker.start()
+        self.worker.run()
 
         self.assertEqual(self.counter, 2)
 
@@ -62,7 +64,7 @@ class WorkerTest(unittest.TestCase):
         self.queue.put((self.worker_callback, [], {}))
         self.queue.put((self.worker_callback, [], {}))
         self.queue.put((self.worker_callback, [], {}))
-        self.worker.start()
+        self.worker.run()
 
         self.assertEqual(self.counter, 3)
 
@@ -70,7 +72,49 @@ class WorkerTest(unittest.TestCase):
         self.tasks = 1
         self.queue.put((self.worker_callback_raise_exception, [], {}))
         self.worker.exception_func = self.worker_exception_callback
-        self.worker.start()
+        self.worker.run()
 
         self.assertEqual(self.exception_counter, 1)
 
+class LockingDictionaryTest(unittest.TestCase):
+
+    def setUp(self):
+        self.ld = LockingDictionary()
+
+    def test_write(self):
+        self.ld.write('foo', 'bar')
+
+        self.assertTrue('foo' in self.ld._dictionary)
+        self.assertEqual('bar', self.ld._dictionary['foo'])
+
+    def test_read(self):
+        self.ld.write('foo', 'bar')
+
+        self.assertEqual('bar', self.ld.read('foo'))
+
+    def test_remove(self):
+        self.ld.write('foo', 'bar')
+        self.ld.remove('foo')
+
+        self.assertFalse('foo' in self.ld._dictionary)
+
+    def test_keys(self):
+        self.ld.write('foo', None)
+        self.ld.write('bar', None)
+
+        self.assertTrue('foo' in self.ld.keys())
+        self.assertTrue('bar' in self.ld.keys())
+
+    def test_items(self):
+        self.ld.write('foo', 'bar')
+        self.ld.write('heart', 'beat')
+
+        self.assertTrue(('foo', 'bar') in self.ld.items())
+        self.assertTrue(('heart', 'beat') in self.ld.items())
+
+    def test_exists(self):
+        self.ld.write('foo', 'bar')
+        self.ld.write('heart', 'beat')
+
+        self.assertTrue(self.ld.exists('foo'))
+        self.assertTrue(self.ld.exists('heart'))
