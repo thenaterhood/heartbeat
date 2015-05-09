56d51c6e92f6b39c67651748bfd3ec21a08c6df3
Nate Levesque <public@thenaterhood.com>
2015-05-08 20:52:49 -0400

    Fix dependencies and version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if (sys.version_info < (3, 4)):
     install_requires.append('enum34')
 
 setup(name='Heartbeat',
-    version='3.0.0',
+    version='2.4.0',
     description='Heartbeat monitoring tool',
     author='Nate Levesque',
     author_email='public@thenaterhood.com',
