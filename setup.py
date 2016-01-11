e0c28d3b2ab1011564c110051084a214e9883541
Nate Levesque <public@thenaterhood.com>
2016-01-11 16:33:33 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ else:
 
 
 setup(name='heartbeat',
-    version='2.8.0',
+    version='2.9.0',
     description='Minimalist network monitoring tool',
     author='Nate Levesque',
     author_email='public@thenaterhood.com',
