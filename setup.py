071da537fcdbc08e614626855e57c12f9d5edd53
Nate Levesque <public@thenaterhood.com>
2016-01-29 20:43:35 -0500

    Fix failing build

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if (sys.version_info < (3, 3)):
     install_requires.append("pymlconf <= 0.3.20")
 
 setup(name='heartbeat',
-    version='3.4.4',
+    version='3.4.5',
     description='Minimalist network monitoring tool',
     author='Nate Levesque',
     author_email='public@thenaterhood.com',
