9d9e0a149835216fb1c03c5c99c1fbbd0be698a5
Nate Levesque <public@thenaterhood.com>
2016-02-05 15:48:20 -0500

    Expand tests for heartbeat

--- test/pluggable/test_heartbeat.py
+++ test/pluggable/test_heartbeat.py
@@ -10,11 +10,12 @@ else:
     from unittest.mock import ANY
 
 from heartbeat.pluggable.heartbeat import Heartbeat, Monitor
-from heartbeat.network import SocketBroadcaster, SocketListener
+from heartbeat.pluggable.heartbeat import Pulse, PulseMonitor
+from heartbeat.network import SocketBroadcaster, SocketListener, NetworkInfo
 from heartbeat.modules import EventServer
-from heartbeat.platform import Event
+from heartbeat.platform import Event, Topics
 from heartbeat.monitoring import MonitorHandler, MonitorType
-from heartbeat.multiprocessing import Cache
+from heartbeat.multiprocessing import Cache, BackgroundTimer
 from pymlconf import ConfigManager
 
 import datetime
@@ -47,6 +48,25 @@ class TestHeartbeat(unittest.TestCase):
         self.hb._beat()
 
         self.hb.bcaster.push.assert_called_once_with(ANY)
+
+
+class TestPulse(unittest.TestCase):
+
+    def setUp(self):
+        self.timer = Mock(name='timer', spec=BackgroundTimer)
+        self.netinfo = Mock(name='network', spec=NetworkInfo)
+        self.hb = Pulse(self.timer, self.netinfo)
+
+    def test_instantiate(self):
+        hb = Heartbeat(self.timer, self.netinfo)
+
+    def test_beat(self):
+        func = MagicMock(return_value=None)
+        self.hb.callback = func
+        self.hb._beat()
+
+        func.assert_called_once_with(ANY)
+
 
 class TestMonitor(unittest.TestCase):
 
@@ -82,6 +102,73 @@ class TestMonitor(unittest.TestCase):
                 }
 
         self.assertEqual(correct, prods)
+
+    def test__log_host(self):
+
+        self.monitor.cache.write = MagicMock(return_value=None)
+        self.monitor.cache.keys = MagicMock(return_value=[])
+        self.monitor.callback = MagicMock()
+
+        self.monitor._log_host('foo.example.com')
+        self.monitor.cache.write.assert_called_with('foo.example.com', ANY)
+        self.monitor.callback.assert_called_with(ANY)
+
+        self.monitor._log_host('foo.example.com')
+        self.monitor.cache.write.assert_called_with('foo.example.com', ANY)
+
+    def test_cleanup_hosts(self):
+
+        self.monitor.cache.write = MagicMock(return_value=None)
+        self.monitor.cache.keys = MagicMock(return_value=['foo.example.com'])
+        self.monitor.cache.items = MagicMock(return_value=[('foo.example.com', 1453848040.6469207)])
+        self.monitor.callback = MagicMock()
+        self.monitor.cache.read = MagicMock(return_value=datetime.datetime(year=1970, month=1, day=1))
+
+        cb = MagicMock(return_value=None)
+        self.monitor.cleanup_hosts(cb)
+        cb.assert_called_once_with(ANY)
+
+    def test_cleanup_hosts_no_expired(self):
+        self.monitor.cache.write = MagicMock(return_value=None)
+        self.monitor.cache.keys = MagicMock(return_value=['foo.example.com'])
+        self.monitor.cache.items = MagicMock(return_value=[('foo.example.com', time())])
+        self.monitor.callback = MagicMock()
+        self.monitor.cache.read = MagicMock(return_value=datetime.datetime(year=1970, month=1, day=1))
+
+        cb = MagicMock(return_value=None)
+        self.monitor.cleanup_hosts(cb)
+
+
+class TestPulseMonitor(unittest.TestCase):
+
+    def setUp(self):
+        self.cache = Mock(name='cache', spec=Cache)
+        self.monitor = PulseMonitor(
+                cache=self.cache,
+                )
+
+    def test_init(self):
+        monitor = PulseMonitor(
+                cache=self.cache,
+                )
+
+        self.assertEqual(self.cache, monitor.cache)
+
+    def test_get_producers(self):
+        prods = self.monitor.get_producers()
+        correct = {
+                MonitorType.PERIODIC: self.monitor.cleanup_hosts
+                }
+
+        self.assertEqual(correct, prods)
+
+    def test_get_subscriptions(self):
+        subs = self.monitor.get_subscriptions()
+        correct = {
+                Topics.HEARTBEAT: self.monitor.receive
+                }
+
+        self.assertEqual(correct, subs)
 
     def test__log_host(self):
 
