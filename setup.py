fdf6ba83a5091a6876689ca05842b293f38d0e65
Nate Levesque <public@thenaterhood.com>
2016-02-06 17:36:58 -0500

    Bump version to v3.8.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if (sys.version_info < (3, 3)):
     install_requires.append("pymlconf <= 0.3.20")
 
 setup(name='heartbeat',
-    version='3.8.1',
+    version='3.8.2',
     description='Minimalist network monitoring tool',
     author='Nate Levesque',
     author_email='public@thenaterhood.com',
