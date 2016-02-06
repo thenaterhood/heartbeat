c6420d176f1ed4c0c932d632c8621494b9968677
Nate Levesque <public@thenaterhood.com>
2016-02-06 16:26:31 -0500

    Add tests for new optional Plugin methods

--- test/plugin/test_plugin.py
+++ test/plugin/test_plugin.py
@@ -1,9 +1,9 @@
 import unittest
 import sys
 if sys.version_info < (3, 3):
-    from mock import Mock
+    from mock import Mock, MagicMock
 else:
-    from unittest.mock import Mock
+    from unittest.mock import Mock, MagicMock
 
 from heartbeat.plugin import Plugin, PluginRegistry
 from pymlconf import ConfigManager
@@ -24,6 +24,32 @@ class TestPlugin(unittest.TestCase):
         Test the stub method correctly returns an empty dict
         """
         self.assertEqual(self.plugin.get_producers(), {})
+
+    def test_get_services(self):
+        self.assertEqual(self.plugin.get_services(), [])
+
+    def test_get_dependencies(self):
+        self.assertEqual(self.plugin.get_required_services(), [])
+
+    def test_check_empty_dependencies(self):
+        self.assertTrue(self.plugin.requirements_satisfied([]))
+        self.assertTrue(self.plugin.requirements_satisfied(None))
+        self.assertTrue(self.plugin.requirements_satisfied(['foo', 'bar']))
+
+    def test_check_dependencies_satisfied(self):
+        self.plugin.get_required_services = MagicMock(return_value=['foo', 'bar'])
+        self.assertTrue(self.plugin.requirements_satisfied(['foo', 'bar']))
+        self.assertTrue(self.plugin.requirements_satisfied(['foo', 'foo', 'bar']))
+        self.assertTrue(self.plugin.requirements_satisfied(['bar', 'foo']))
+        self.assertTrue(self.plugin.requirements_satisfied(['foo', 'bar', 'baz']))
+
+    def test_check_dependencies_not_satisfied(self):
+        self.plugin.get_required_services = MagicMock(return_value=['foo', 'bar'])
+        self.assertFalse(self.plugin.requirements_satisfied([]))
+        self.assertFalse(self.plugin.requirements_satisfied(['foo']))
+        self.assertFalse(self.plugin.requirements_satisfied(['bar']))
+        self.assertFalse(self.plugin.requirements_satisfied(['foo', 'baz']))
+        self.assertFalse(self.plugin.requirements_satisfied(None))
 
 class TestPluginRegistry(unittest.TestCase):
 
